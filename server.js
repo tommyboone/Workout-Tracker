4752a913b13a5e206ee96995a07c954ab49affae
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-04-13 13:40:21 -0500

    server up and running on 8080

---
+++ server.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan('dev'));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//MONGODB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+//Routes
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
+
+
+//Starts server listening on PORT
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
+});
