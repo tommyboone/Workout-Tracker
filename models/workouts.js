6d8077c549a09af10ccdc67530dd67a967b05118
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-04-14 17:49:10 -0500

    set up Workout Schema

---
+++ models/workouts.js
@@ -0,0 +1,61 @@
+  
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "What type of exercise?"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "What is the name of this exercise?"
+        },
+        duration: {
+          type: Number,
+          required: "How long are you exercising? (in minutes)"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
+    }
+  }
+);
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
